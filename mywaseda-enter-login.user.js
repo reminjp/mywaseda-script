155bbf47a7eb87119c55851664df5f91a68b9753
remin <reminjp@gmail.com>
2017-12-23 17:28:44 +0900

    update UserScript header

--- mywaseda-enter-login.user.js
+++ mywaseda-enter-login.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        MyWaseda Enter Login
-// @namespace   http://kize.net
+// @namespace   https://remin.jp
+// @version     1.0
+// @author      remin
 // @description Scripts to enable enter key on MyWaseda login page
 // @include     https://iaidp.ia.waseda.jp/idp/profile/SAML2/Redirect/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @version     1.0
 // ==/UserScript==
 
 jQuery(function () {
